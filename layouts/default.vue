26b31e6378952a7deb6ded1ccac27ca3f06d6dd3
Surya Rajendhran <cielograndesurya@gmail.com>
2020-12-11 12:14:01 +0530

    Mobile and desktop nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
     <v-navigation-drawer
       v-model="drawer"
       :mini-variant.sync="mini"
-      permanent
+      :permanent="!is_mobile"
       app
     >
       <v-list-item style="height: 64px" class="px-2">
@@ -40,10 +40,11 @@
       </v-list>
     </v-navigation-drawer>
     <v-app-bar elevate-on-scroll app>
+      <v-app-bar-nav-icon v-if="is_mobile" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-spacer></v-spacer>
-      <v-btn class="mr-2" icon><v-icon>mdi-bell</v-icon></v-btn>
-      <v-btn class="mr-2" text>Go to docs</v-btn>
-      <v-btn class="ml-2" icon><v-icon>mdi-account</v-icon></v-btn>
+      <v-btn class="mr-1" icon><v-icon>mdi-bell</v-icon></v-btn>
+      <v-btn class="mr-1" text>Go to docs</v-btn>
+      <v-btn class="ml-1" icon><v-icon>mdi-account</v-icon></v-btn>
     </v-app-bar>
     <v-main>
       <v-container>
@@ -56,13 +57,14 @@
 <script>
 export default {
   data() {
-    var mini = false
+    var is_mobile = false
     if (window.innerHeight < 800 || window.innerWidth < 400) {
-      mini = true
+      is_mobile = true
     }
     return {
-      drawer: false,
-      mini,
+      is_mobile,
+      drawer: !is_mobile,
+      mini: false,
       items: [
         {
           icon: 'mdi-apps',
