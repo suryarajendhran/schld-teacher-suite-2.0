dee8893565220dc6a43c231bb58526ff45bce141
Surya Rajendhran <cielograndesurya@gmail.com>
2020-12-11 15:31:16 +0530

    Change title

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,8 +9,8 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - teacher-suite',
-    title: 'Teacher Suite - Scholared',
+    titleTemplate: '%s - Scholared',
+    title: 'Teacher Suite',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
