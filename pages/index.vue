1a953c092866e8858e9971c3a1578f5d4723d28b
Surya Rajendhran <cielograndesurya@gmail.com>
2020-12-13 11:21:37 +0530

    Sparkline working

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-col>
+  <div>
     <v-row justify="space-between">
       <v-card class="mt-2" v-for="room in classes" :key="room.cid" width="380">
         <v-card-title primary-title cl>
@@ -21,24 +21,27 @@
         </v-card-actions>
       </v-card>
     </v-row>
-    <v-row>
-      <!-- <v-card class="mt-4 mx-auto" elevation="12" max-width="calc(100%-32px)"> -->
-      <v-sparkline
-        :value="value"
-        :gradient="gradient"
-        :smooth="radius || false"
-        :padding="padding"
-        :line-width="width"
-        :stroke-linecap="lineCap"
-        :gradient-direction="gradientDirection"
-        :fill="fill"
-        :type="type"
-        :auto-line-width="autoLineWidth"
-        auto-draw
-      ></v-sparkline>
-      <!-- </v-card> -->
-    </v-row>
-  </v-col>
+    <div>
+      <v-card
+        class="mt-4 pb-2 mx-auto"
+        elevation="8"
+        max-width="calc(100%-32px)"
+      >
+        <v-sparkline
+          :value="value"
+          :labels="labels"
+          label-size="5"
+          line-width="2"
+          color="#555"
+          height="100"
+          padding="24"
+          stroke-linecap="round"
+          smooth
+        >
+        </v-sparkline>
+      </v-card>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -53,17 +56,6 @@ export default {
       ['#f72047', '#ffd200', '#1feaea'],
     ]
     return {
-      width: 2,
-      radius: 10,
-      padding: 8,
-      lineCap: 'round',
-      gradient: gradients[5],
-      value: [0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0],
-      gradientDirection: 'top',
-      gradients,
-      fill: false,
-      type: 'trend',
-      autoLineWidth: false,
       classes: [
         {
           cid: '18474uy',
@@ -94,31 +86,31 @@ export default {
         { label: '12th', questions: '120' },
         { label: '13th', questions: '104' },
         { label: '14th', questions: '144' },
-        { label: '15th', questions: '122' },
+        { label: '15th', questions: '102' },
         { label: '16th', questions: '99' },
         { label: '17th', questions: '117' },
       ],
     }
   },
   computed: {
-    // labels() {
-    //   let labels = []
-    //   if (this.activity) {
-    //     this.activity.forEach((day) => {
-    //       labels.push(day.label)
-    //     })
-    //   }
-    //   return labels
-    // },
-    // value() {
-    //   let values = []
-    //   if (this.activity) {
-    //     this.activity.forEach((day) => {
-    //       values.push(parseInt(day.questions))
-    //     })
-    //   }
-    //   return values
-    // },
+    labels() {
+      let labels = []
+      if (this.activity) {
+        this.activity.forEach((day) => {
+          labels.push(day.label)
+        })
+      }
+      return labels
+    },
+    value() {
+      let values = []
+      if (this.activity) {
+        this.activity.forEach((day) => {
+          values.push(parseInt(day.questions))
+        })
+      }
+      return values
+    },
   },
 }
 </script>
