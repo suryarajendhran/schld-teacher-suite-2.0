3f774625d5d2e626d57d705d4c0f48f3e8d90916
Surya Rajendhran <cielograndesurya@gmail.com>
2020-12-13 11:06:23 +0530

    Sparkline debugging

--- pages/index.vue
+++ pages/index.vue
@@ -22,14 +22,21 @@
       </v-card>
     </v-row>
     <v-row>
-      <v-card class="mt-4 mx-auto" elevation="12" max-width="calc(100%-32px)">
-        <v-sparkline
-          :value="value"
-          line-width="2"
-          padding="16"
-        >
-        </v-sparkline>
-      </v-card>
+      <!-- <v-card class="mt-4 mx-auto" elevation="12" max-width="calc(100%-32px)"> -->
+      <v-sparkline
+        :value="value"
+        :gradient="gradient"
+        :smooth="radius || false"
+        :padding="padding"
+        :line-width="width"
+        :stroke-linecap="lineCap"
+        :gradient-direction="gradientDirection"
+        :fill="fill"
+        :type="type"
+        :auto-line-width="autoLineWidth"
+        auto-draw
+      ></v-sparkline>
+      <!-- </v-card> -->
     </v-row>
   </v-col>
 </template>
@@ -37,7 +44,26 @@
 <script>
 export default {
   data() {
+    const gradients = [
+      ['#222'],
+      ['#42b3f4'],
+      ['red', 'orange', 'yellow'],
+      ['purple', 'violet'],
+      ['#00c6ff', '#F0F', '#FF0'],
+      ['#f72047', '#ffd200', '#1feaea'],
+    ]
     return {
+      width: 2,
+      radius: 10,
+      padding: 8,
+      lineCap: 'round',
+      gradient: gradients[5],
+      value: [0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0],
+      gradientDirection: 'top',
+      gradients,
+      fill: false,
+      type: 'trend',
+      autoLineWidth: false,
       classes: [
         {
           cid: '18474uy',
@@ -75,24 +101,24 @@ export default {
     }
   },
   computed: {
-    labels() {
-      let labels = []
-      if (this.activity) {
-        this.activity.forEach((day) => {
-          labels.push(day.label)
-        })
-      }
-      return labels
-    },
-    value() {
-      let values = []
-      if (this.activity) {
-        this.activity.forEach((day) => {
-          values.push(parseInt(day.questions))
-        })
-      }
-      return values
-    },
+    // labels() {
+    //   let labels = []
+    //   if (this.activity) {
+    //     this.activity.forEach((day) => {
+    //       labels.push(day.label)
+    //     })
+    //   }
+    //   return labels
+    // },
+    // value() {
+    //   let values = []
+    //   if (this.activity) {
+    //     this.activity.forEach((day) => {
+    //       values.push(parseInt(day.questions))
+    //     })
+    //   }
+    //   return values
+    // },
   },
 }
 </script>
